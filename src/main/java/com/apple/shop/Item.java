62ed733501098c961e72edc9fe5a7859148269c8
semin5 <omhmir1@gamil.com>
2026-01-08 00:26:03 +0900

    commit5

---
+++ src/main/java/com/apple/shop/Item.java
@@ -0,0 +1,14 @@
+package com.apple.shop;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Item {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+    String title;
+    Integer price;
+}
