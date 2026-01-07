3807d27a30915c6a7a0c0d5967812a0cb8c5163b
semin5 <omhmir1@gamil.com>
2026-01-07 23:00:06 +0900

    commit1

--- src/main/java/com/apple/shop/ShopApplication.java
+++ src/main/java/com/apple/shop/ShopApplication.java
@@ -1,5 +1,6 @@
 package com.apple.shop;
 
+import ch.qos.logback.core.encoder.JsonEscapeUtil;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +8,19 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ShopApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(ShopApplication.class, args);
+
+		Friend friend = new Friend("lee");
+		System.out.println(friend.name);
 	}
 
+}
+
+class Friend{
+	String name;
+	int age = 20;
+	Friend(String a){
+		this.name = a;
+	}
 }
