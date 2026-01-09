504acc00a29b9d3292c4a15e4273e0e0444068d5
semin5 <omhmir1@gamil.com>
2026-01-10 08:04:18 +0900

    commit14

--- src/main/java/com/apple/shop/ItemController.java
+++ src/main/java/com/apple/shop/ItemController.java
@@ -4,13 +4,11 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 @Controller
 @RequiredArgsConstructor
@@ -42,5 +40,17 @@ public class ItemController {
         itemRepository.save(item);
 
         return "redirect:/list";
+    }
+
+    @GetMapping("/detail/{id}")
+    String detail(@PathVariable Long id, Model model){
+        Optional<Item> result = itemRepository.findById(id);
+        if(result.isPresent()){
+            model.addAttribute("data", result.get());
+
+            return "detail.html";
+        } else{
+            return "redirect:/list";
+        }
     }
 }
