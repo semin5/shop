1b9b42180fb484f3fd408033c52968a4abeb1e0a
semin5 <omhmir1@gamil.com>
2026-01-10 08:13:02 +0900

    commit15

--- src/main/java/com/apple/shop/ItemController.java
+++ src/main/java/com/apple/shop/ItemController.java
@@ -1,12 +1,10 @@
 package com.apple.shop;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.*;
 
@@ -44,6 +42,7 @@ public class ItemController {
 
     @GetMapping("/detail/{id}")
     String detail(@PathVariable Long id, Model model){
+
         Optional<Item> result = itemRepository.findById(id);
         if(result.isPresent()){
             model.addAttribute("data", result.get());
