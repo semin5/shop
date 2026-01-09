996798401386b725e67d1c066c3adddd3c39d6e7
semin5 <omhmir1@gamil.com>
2026-01-09 10:20:42 +0900

    commit12

--- src/main/java/com/apple/shop/Item.java
+++ src/main/java/com/apple/shop/Item.java
@@ -4,14 +4,18 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
 import lombok.ToString;
 
 
 @Entity
 @ToString
+@Getter
+@Setter
 public class Item {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
-    public Long id;
+    private Long id;
     private String title;
     private Integer price;
 
--- src/main/java/com/apple/shop/ItemController.java
+++ src/main/java/com/apple/shop/ItemController.java
@@ -4,9 +4,13 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Controller
 @RequiredArgsConstructor
@@ -18,8 +22,25 @@ public class ItemController {
     String list(Model model){
         List<Item> result = itemRepository.findAll();
         model.addAttribute("items", result);
-        var a = new Item();
 
         return "list.html";
+    }
+
+    @GetMapping("/write")
+    String write(Model model){
+
+        return "write.html";
+    }
+
+    @PostMapping("/add")
+    String addPost(@RequestParam String title,
+                   @RequestParam Integer price){
+
+        Item item = new Item();
+        item.setTitle(title);
+        item.setPrice(price);
+        itemRepository.save(item);
+
+        return "redirect:/list";
     }
 }
